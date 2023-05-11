8bd0c1722b25f3d0e46c9b7b4c7f5036760f20e7
Subhash Vaddella <subhashvaddella@gmail.com>
2023-05-11 15:29:56 +0100

    bug fixes

---
+++ UI/javascript/form-validation.js
@@ -0,0 +1,47 @@
+const passwordField = $("#password"),
+    repeatPasswordField = $("#passwordRepeat"),
+    termsCheckbox = $("#terms");
+
+function validateForm() {
+    if (!isPasswordLengthValid() || !passwordsAreMatch()) {
+        alert("Failed to create account")
+        return false
+    }
+    else if(!termsAreChecked()){
+        alert("Failed to create account")
+        return false
+    }
+    alert("Account created successfully")
+    return true
+}
+
+function isPasswordLengthValid() {
+    const passwordText = passwordField.val()
+    if (passwordText.length > 17 || passwordText.length < 6) {
+        setBorderColour(passwordField, "red")
+        return false
+    }
+    return true
+}
+
+function passwordsAreMatch() {
+    if (passwordField.val() !== repeatPasswordField.val()) {
+        setBorderColour(passwordField, "red")
+        setBorderColour(repeatPasswordField, "red")
+        return false
+    }
+    return true
+}
+
+function termsAreChecked() {
+    if (!termsCheckbox.is(':checked')) {
+        setBorderColour($("#termsAndConditions"), "red")
+        return false
+    }
+    return true
+}
+
+function setBorderColour(field, colour) {
+    field.css("border-style", "solid")
+    field.css("border-color", colour)
+}
